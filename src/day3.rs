174b742b73d8a3a57c02ea72697ed6207a5ad0ae
Riley Avron <ravron@posteo.net>
2020-12-02 21:55:38 -0800

    Day 3

---
+++ src/day3.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+struct Field {
+    lines: Vec<bool>,
+    n: usize,
+    m: usize,
+}
+
+pub fn day3() {
+    let f = File::open("inputs/day3.txt").unwrap();
+    let r = BufReader::new(f);
+
+    let lines: Vec<String> = r.lines().map(|l| l.unwrap()).collect();
+
+    let n = lines[0].len();
+    let m = lines.len();
+    let mut field = Field {
+        lines: vec![false; n * m],
+        n,
+        m,
+    };
+
+    for (row, line) in lines.iter().enumerate() {
+        for (col, char) in line.chars().enumerate() {
+            if char == '#' {
+                field.lines[row * n + col] = true;
+            }
+        }
+    }
+
+    println!("part 1: encountered {} trees", check_slope(&field, 3, 1));
+
+    let part_2_result = check_slope(&field, 1, 1) *
+        check_slope(&field, 3, 1) *
+        check_slope(&field, 5, 1) *
+        check_slope(&field, 7, 1) *
+        check_slope(&field, 1, 2);
+    println!("part 2: result is {}", part_2_result);
+}
+
+fn check_slope(field: &Field, right: usize, down: usize) -> i32 {
+    let mut col = 0;
+    let mut trees = 0;
+    for row in (0..field.m).step_by(down) {
+        if field.lines[row * field.n + col] == true {
+            trees += 1;
+        }
+        col = (col + right) % field.n;
+    }
+    trees
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,6 +10,7 @@ fn main() {
     match day {
         1 => day1::day1(),
         2 => day2::day2(),
+        3 => day3::day3(),
         x => println!("unimplemented day {}", x),
     }
 }
